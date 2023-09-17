1d80bbc8524552683bba2daee15a7e6a00baf09d
Cplhardcore <135324281+Cplhardcore@users.noreply.github.com>
2023-09-17 05:07:53 -0700

    Update 4RPlayer.hpp

--- 4RPlayer.hpp
+++ 4RPlayer.hpp
@@ -9,7 +9,7 @@ class CAManBase: Man
 			class 4R_Tag_MIST
 			{
 				displayName = "MIST Card";
-				condition = "'4R_MIST_TAG' in (items player)";
+				condition = "this && ("4R_MIST_TAG" in (items player)";
 				statement = "createDialog '4R_MIST_Tag'; _player removeItem '4R_MIST_TAG'";
 				icon="\16aaMIST\data\MISTCARD.paa";
 			};
